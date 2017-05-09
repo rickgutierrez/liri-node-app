e296eb27e9165fc291a522641b6a8cf05b2c9cba
rickgutierrez <rickgut945@gmail.com>
2017-05-08 21:47:17 -0700

    Add files via upload

---
+++ lirihomework.js
@@ -0,0 +1,120 @@
+// Node module imports needed to run the functions
+	var fs = require("fs"); //reads and writes files
+	var request = require("request");
+	var keys = require("./keys.js");
+	var twitter = require("twitter");
+	var spotify = require ("spotify");
+	var liriArgument = process.argv[2];
+// ---------------------------------------------------------------------------------------------------------------
+	
+// ---------------------------------------------------------------------------------------------------------------
+// Functions
+	// Movie function, uses the Request module to call the OMDB api
+	function movieThis(){
+		var movie = process.argv[3];
+		if(!movie){
+			movie = "the wolf of wall street";
+		}
+		params = movie
+		request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var movieObject = JSON.parse(body);
+			
+				var movieResults =
+				"------------------------------ begin ------------------------------" + "\r\n"
+				"Title: " + movieObject.Title+"\r\n"+
+				"Year: " + movieObject.Year+"\r\n"+
+				"Imdb Rating: " + movieObject.imdbRating+"\r\n"+
+				"Country: " + movieObject.Country+"\r\n"+
+				"Language: " + movieObject.Language+"\r\n"+
+				"Plot: " + movieObject.Plot+"\r\n"+
+				"Actors: " + movieObject.Actors+"\r\n"+
+				"Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\r\n"+
+				"Rotten Tomatoes URL: " + movieObject.tomatoURL + "\r\n" + 
+				"------------------------------ fin ------------------------------" + "\r\n";
+				console.log(movieResults);
+				log(movieResults); //  log function
+			} else {
+				console.log("Error :"+ error);
+				return;
+			}
+		});
+	};
+	//  Twitter api
+	function myTweets() {
+		var client = new twitter({
+			consumer_key: keys.twitterKeys.consumer_key,
+			consumer_secret: keys.twitterKeys.consumer_secret,
+			access_token_key: keys.twitterKeys.access_token_key,
+			access_token_secret: keys.twitterKeys.access_token_secret, 
+		});
+		var twitterUsername = process.argv[3];
+		if(!twitterUsername){
+			twitterUsername = "";  //Need twitter account???
+		}
+		params = {screen_name: twitterUsername};
+		client.get("statuses/user_timeline/", params, function(error, data, response){
+			if (!error) {
+				for(var i = 0; i < data.length; i++) {
+					//console.log(response); // Show the full response in the terminal
+					var twitterResults = 
+					"@" + data[i].user.screen_name + ": " + 
+					data[i].text + "\r\n" + 
+					data[i].created_at + "\r\n" + 
+					"------------------------------ " + i + " ------------------------------" + "\r\n";
+					console.log(twitterResults);
+					log(twitterResults); // calling log function
+				}
+			}  else {
+				console.log("Error :"+ error);
+				return;
+			}
+		});
+	}
+					//Spotify api
+	function spotifyThisSong(songName) {
+		var songName = process.argv[3];
+		if(!songName){
+			songName = "I'm in Love with the Coco";
+		}
+		params = songName;
+		spotify.search({ type: "track", query: params }, function(err, data) {
+			if(!err){
+				var songInfo = data.tracks.items;
+				for (var i = 0; i < 5; i++) {
+					if (songInfo[i] != undefined) {
+						var spotifyResults =
+						"Artist: " + songInfo[i].artists[0].name + "\r\n" +
+						"Song: " + songInfo[i].name + "\r\n" +
+						"Album the song is from: " + songInfo[i].album.name + "\r\n" +
+						"Preview Url: " + songInfo[i].preview_url + "\r\n" + 
+						"------------------------------ " + i + " ------------------------------" + "\r\n";
+						console.log(spotifyResults);
+						log(spotifyResults); // calling log function
+					}
+				}
+			}	else {
+				console.log("Error :"+ err);
+				return;
+			}
+		});
+	};
+	
+	function doWhatItSays() {
+		fs.readFile("random.txt", "utf8", function(error, data){
+			if (!error) {
+				doWhatItSaysResults = data.split(",");
+				spotifyThisSong(doWhatItSaysResults[0], doWhatItSaysResults[1]);
+			} else {
+				console.log("Error occurred" + error);
+			}
+		});
+	};
+	
+	function log(logResults) {
+	  fs.appendFile("log.txt", logResults, (error) => {
+	    if(error) {
+	      throw error;
+	    }
+	  });
+	}
